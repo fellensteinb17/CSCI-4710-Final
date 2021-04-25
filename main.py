7fadfe88d6f7cf1036a471c16d6e2ca9f11360c9
BrandonFellenstein <fellensteinb17@students.ecu.edu>
2021-04-25 12:04:44 -0400

    rework

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for,  flash
+import os
+import json
+import pandas as pd
+import requests
+
+IMAGE_FOLDER = os.path.join('static', 'images')
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+
+app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+
+
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+    
+@app.route('/About')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run()
