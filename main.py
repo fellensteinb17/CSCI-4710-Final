6ce76237136537f655ed3d475b30ef95b0d7e6b1
stanleyja19 <72470560+stanleyja19@users.noreply.github.com>
2021-04-26 02:56:45 -0400

    created the temporary random god choice
    randomly chooses a god when opened. buttons still do not work

--- main.py
+++ main.py
@@ -1,25 +1,75 @@
 from flask import Flask, render_template, request, redirect, url_for,  flash
+from flask_sqlalchemy import SQLAlchemy
+from  sqlalchemy.sql.expression import func
 import os
 import json
+import csv
 import pandas as pd
 import requests
 
-IMAGE_FOLDER = os.path.join('static', 'images')
-
+# get current app directory
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+# define SQLAlchemy URL, a configuration parameter
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'smite.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+IMAGE_FOLDER = os.path.join('static', 'images')
 
 app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
 
+class Gods(db.Model):
+	__tablename__ = 'Gods'
+	image = db.Column(db.String(255))
+	name = db.Column(db.String(20), primary_key=True)
+	pantheon = db.Column(db.String(20))
+	damage = db.Column(db.String(10))
+	role = db.Column(db.String(10))
+	def __repr__(self):
+		return '<Gods %r >' % self.__dict__
 
+class Relics(db.Model):
+	__tablename__ = 'Relics'
+	image = db.Column(db.String(255))
+	name = db.Column(db.String(20), primary_key=True)
+	def __repr__(self):
+		return '<Relics %r >' % self.__dict__
 
 @app.route('/', methods=['GET', 'POST'])
-def main():
-    
+def index():    
     smite_logo = os.path.join(app.config['IMAGE_FOLDER'], 'SmiteLogo.png')
 
-    return render_template('home.html', smite_logo = smite_logo)
+    db.drop_all()
+    db.create_all()
+    with open('smite_gods.csv', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+        	newgod = Gods(image=row[0], name=row[1], pantheon=row[2], damage=row[3], role=row[4])
+        	db.session.add(newgod)
+        	db.session.commit()
+
+    #db.drop_all()
+    db.create_all()
+    with open('relics.csv', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+        	newrelic = Relics(image=row[0], name=row[1])
+        	db.session.add(newrelic)
+        	db.session.commit()
+
+    db.create_all()
+
+    random = Gods.query.order_by(func.random()).limit(1)
+    #god_icon = os.path.join(app.config['IMAGE_FOLDER'], 'Gods', random["image"])
+    #query_all_gods = Gods.query.filter_by(role=' Mage').all()
+    #query_all_relics = Relics.query.all()
+    #return render_template('home.html', smite_logo = smite_logo, column_gods=Gods.__table__.columns.keys(), data_gods= query_all_gods, column_relics=Relics.__table__.columns.keys(), data_relics= query_all_relics)
+    return render_template('home.html', smite_logo = smite_logo, column_god=Gods.__table__.columns.keys(), data_god=random)
     
 @app.route('/About')
 def about():
